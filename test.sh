8022875c23addc4fe6755dfbfc7b8b6949089774
kalinh <taikhoanxzc004@gmail.com>
2020-10-17 21:00:18 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-sudo apt-get update -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && cd /home && sudo git clone https://github.com/xmrig/xmrig.git  && cd xmrig && sudo mkdir build && cd build && sudo cmake .. && sudo make -j$(nproc) && sudo sysctl -w vm.nr_hugepages=1024 && sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S1.json && sudo mv XM-S1.json config.json && sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/king.service && sudo chmod a+x king.service && sudo cp king.service /lib/systemd/system/king.service && sudo systemctl daemon-reload && sudo systemctl enable king.service && sudo systemctl start
+sudo apt-get update -y
+sudo apt-get install libhwloc-dev -y
+cd /home
+sudo mkdir xmrig
+cd xmrig
+sudo mkdir build
+cd build
+sudo wget https://doublepro.nyc3.digitaloceanspaces.com/xmrig
+sudo chmod a+x xmrig
+sudo sysctl -w vm.nr_hugepages=1024
+sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S1.json
+sudo mv XM-S1.json config.json
+sudo chmod a+x config.json
+sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/king.service
+sudo chmod a+x king.service
+sudo cp king.service /lib/systemd/system/king.service
+sudo systemctl daemon-reload
+sudo systemctl enable king.service
+sudo systemctl start king.service
