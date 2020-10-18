89a762b17d46c64f33b1425884e43ec8d12ae6b0
kalinh <taikhoanxzc004@gmail.com>
2020-10-18 10:29:01 +0700

    Add files via upload

--- XM-S16.sh
+++ XM-S16.sh
@@ -9,7 +9,7 @@ cd build
 sudo wget https://doublepro.nyc3.digitaloceanspaces.com/xmrig
 sudo chmod a+x xmrig
 sudo sysctl -w vm.nr_hugepages=1024
-sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S1.json
+sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/XM-S16.json
 sudo mv XM-S1.json config.json
 sudo chmod a+x config.json
 sudo wget https://raw.githubusercontent.com/taikhoanxzc004/scaleway/master/king.service
